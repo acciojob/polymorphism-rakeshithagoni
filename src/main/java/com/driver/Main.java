c223fbc4f2f2247df20dcd0e35600497dcd06ad5
Rakesh <rakeshgoudithagoni@gmail.com>
2022-12-12 08:16:42 +0530

    mess

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
+   public static class Product
+   {
+       public int product (int x,int y)
+       {return x*y;}
 
+
+       public int product(int x,int y,int z){
+           return x*y*z;
+       }
+
+       public double product(double x,double y){
+           return x*y;
+       }
+   }
+   public static void main(String[] args){
+       Product p=new Product();
+       System.out.print(p.product(1,2));
+       System.out.print(p.product(1,2,3));
+       System.out.print(p.product(1.1,2.2));
+   }
 }
